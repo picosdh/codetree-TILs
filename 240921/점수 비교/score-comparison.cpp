6f96227057ef688334357e03ad98c82625d306dc
picosdh <ssambbongdongwoo@gmail.com>
2024-09-21 14:50:14 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240921/점수 비교/score-comparison.cpp
+++ 240921/점수 비교/score-comparison.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int a1, a2;
     int b1, b2;
-    cin << a1 << a2 << b1 << b2;
+    cin >> a1 >> a2 >> b1 >> b2;
     if (a1>a2 & b1>b2){
         cout << 1;
     }
