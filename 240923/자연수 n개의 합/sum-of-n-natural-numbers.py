532db718837b6b0e451f130c0c3867a64258d7a2
picosdh <ssambbongdongwoo@gmail.com>
2024-09-23 14:56:16 +0900

    Title: 자연수 n개의 합, Time: 5063ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 240923/자연수 n개의 합/sum-of-n-natural-numbers.py
+++ 240923/자연수 n개의 합/sum-of-n-natural-numbers.py
@@ -2,7 +2,7 @@ n = int(input())
 ans = 0
 def go(n):
     return (n*(n+1))*1/2
-for i in range(int(n**(0.5))-int(n**0.2), 141421356):
+for i in range(int(n**0.2), 141421356):
     if go(i) <= n:
         ans = i
     else:
