192926e7d67d75ba55073bbbabee69d1c1f9a97e
picosdh <ssambbongdongwoo@gmail.com>
2024-11-02 14:44:37 +0900

    Title: 윤년 횟수 구하기, Time: 70ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241102/윤년 횟수 구하기/number-of-leap-years.py
@@ -0,0 +1,12 @@
+n = int(input())
+cnt = 0
+for i in range(1, n+=1):
+    if i%4 == 0:
+        if i%100 == 0:
+            if i%400 != 0:
+                continue
+            else:
+                cnt += 1
+        else:
+            cnt += 1
+print(cnt)
