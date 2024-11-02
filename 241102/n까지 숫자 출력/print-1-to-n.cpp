f0c13d4e68bdd622297eac9b20e4fec9cec2818d
picosdh <ssambbongdongwoo@gmail.com>
2024-11-02 14:30:57 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241102/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    int i = 1;
+    int n;
+    scanf("%d", &n);
+    while(i <= n){
+        printf("%d\n", i);
+        i++;
+    }
+    return 0;
+}
