9c50974ec78c64e6905f0dc1659d855317fbf7b6
picosdh <ssambbongdongwoo@gmail.com>
2024-11-02 15:38:57 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241102/별표 출력하기 12/print-star-12.cpp
+++ 241102/별표 출력하기 12/print-star-12.cpp
@@ -8,22 +8,23 @@ int main() {
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= n; j++){
             if(j%2 == 1){
-                if(i>1){
-                    printf("  ");
+                if(i == 1){
+                    printf("* ");
                 }
                 else{
-                    printf("* ");
+                    printf("  ");
                 }
             }
             else{
                 if(i>j){
-                    printf("* ");
+                    printf("  ");
                 }
                 else{
-                    printf("  ");
+                    printf("* ");
                 }
             }
         }
+        printf("\n");
     }
     return 0;
 }
