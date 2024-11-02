0257e4d48e9f399aea96054b6bf1997c3debd049
picosdh <ssambbongdongwoo@gmail.com>
2024-11-02 15:31:33 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241102/별표 출력하기 12/print-star-12.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            if(j%2 == 1){
+                if(i>1){
+                    printf("  ");
+                }
+                else{
+                    printf("* ");
+                }
+            }
+            else{
+                if(i>j){
+                    printf("* ");
+                }
+                else{
+                    printf("  ");
+                }
+            }
+        }
+    }
+    return 0;
+}
