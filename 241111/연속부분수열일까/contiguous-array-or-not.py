6f1b07c1c9c30d50afb731b50e1eeb8ebcff0b67
picosdh <ssambbongdongwoo@gmail.com>
2024-11-11 16:18:41 +0900

    Title: 연속부분수열일까, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241111/연속부분수열일까/contiguous-array-or-not.py
+++ 241111/연속부분수열일까/contiguous-array-or-not.py
@@ -3,11 +3,12 @@ l = list(map(int, input().split()))
 l1 = list(map(int, input().split()))
 res = 0
 for i in range(len(l)):
-    if len(l) - i + 1 >= len(l1):
-        if l[i] == l[res] and res == 0:
+    if len(l) - i + 1 >= len(l1) - res + 1:
+
+        if l[i] == l1[res] and res == 0:
             res += 1
         else:
-            if l[i] == l[res]:
+            if l[i] == l1[res]:
                 res += 1
             else:
                 res = 0
