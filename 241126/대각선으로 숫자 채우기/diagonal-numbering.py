f9b33aa0b26a1826ece326aed6ba1110ca4556b6
picosdh <ssambbongdongwoo@gmail.com>
2024-11-26 17:23:59 +0900

    Title: 대각선으로 숫자 채우기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241126/대각선으로 숫자 채우기/diagonal-numbering.py
+++ 241126/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -1,17 +1,23 @@
 n, m = map(int, input().split())
 l = [[0 for _ in range(m)]for _ in range(n)]
 plus = 0
-k = 0
+lp = 0
 for i in range(m):
-    l[0][i] = 1 + plus
-    plus += 1 + k
-    k += 1
+    if (1 + plus) - lp <= n:
+        l[0][i] = 1 + plus
+        lp = 1 + plus
+        plus += 1 + i
+    else:
+        l[0][i] = lp + n
+        lp += n
 for i in range(1, n):
     for j in range(m):
         if j != m-1:
             l[i][j] = l[i-1][j+1] + 1
+        elif i + 1 == n and j + 1 == m:
+            l[i][j] = l[i][j-1] + 1
         else:
-            l[i][j] = l[i-1][j] + m - i + 1
+            l[i][j] = l[i-1][j] + m - n + i
 for i in range(n):
     print(*l[i], end=' ')
     print()
