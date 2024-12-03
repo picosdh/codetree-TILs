cc9bb6725e71187191d293a8677f481dcacb8314
picosdh <ssambbongdongwoo@gmail.com>
2024-12-03 16:55:30 +0900

    Title: 최고의 33위치, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241203/최고의 33위치/best-place-of-33.py
+++ 241203/최고의 33위치/best-place-of-33.py
@@ -1,14 +1,18 @@
-def go(n, l):
+def go(l):
     cnt = 0
-    for i in range(n):
-        for j in range(n):
-            cnt += l[i, j]
+    for i in range(3):
+        for j in range(3):
+            cnt += l[i][j]
     return cnt
-n = map(int, input().split())
+n = int(input())
 l = [list(map(int, input().split())) for _ in range(n)]
 maxV = 0
 for i in range(n-2):
     for j in range(n-2):
-        if maxV <= def(l[i:][j:]):
-            maxV = def(l[i:][j:])
+        nl = [[0 for _ in range(3)]for _ in range(3)] 
+        for k in range(3):
+            for o in range(3):
+                nl[k][o] = l[i+k][j+o]
+        if maxV <= go(nl):
+            maxV = go(nl)
 print(maxV)
